3a0ae9afd1b390ad4c402f5e4f841b575ed8d36c
raghavpatel220292 <raghav2202@zohomail.in>
2026-06-02 14:58:43 +0530

    test

--- test_rg.tf
+++ test_rg.tf
@@ -2,3 +2,8 @@ resource "azurerm_resouce_group" "rg"{
   name = "dev-test1"
   location = "centralindia"
   }
+
+  resource "azurerm_resouce_group" "rg2"{
+  name = "dev-test21"
+  location = "centralindia"
+  }
