06692b3c7eaf85ce497df0e0feca2f1d588ceaae
raghavpatel220292 <raghav2202@zohomail.in>
2026-06-02 16:58:29 +0530

    stg added

--- stg.tf
+++ stg.tf
@@ -6,3 +6,12 @@ account_tier ="Standard"
 account_replication_type = "LRS"
 
 }
+
+resource "azurerm_storage_account" "stg2" {
+name = "stg245sfasf5dsd"
+location = "centralindia"
+resource_group_name = "test21"
+account_tier ="Standard"
+account_replication_type = "LRS"
+
+}
