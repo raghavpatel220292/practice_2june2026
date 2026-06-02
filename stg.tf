8e5c3e9a83407727c8c62d9c57e4963998f70221
raghavpatel220292 <raghav2202@zohomail.in>
2026-06-02 17:11:46 +0530

    Update stg.tf

--- stg.tf
+++ stg.tf
@@ -1,7 +1,7 @@
-resource "azurerm_storage_account" "stg" {
-name = "stg2455dsd"
+resource "azurerm_storage_account" "stg99" {
+name = "stg2455dsd99"
 location = "centralindia"
-resource_group_name = "test1"
+resource_group_name = "test9991"
 account_tier ="Standard"
 account replication_type = "LRS"
 }
